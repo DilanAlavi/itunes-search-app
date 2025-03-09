88134dfaa52d7b0eee1f77f67c8a003b819cc0c9
DILAN ALVARO ALAVI GUTIERREZ <dilanalavi11@gmail.com>
2025-03-09 07:27:42 -0400

    Actualizar jsonpRequest para devolver una Promesa

--- src/Services/iTunesService.js
+++ src/Services/iTunesService.js
@@ -1,12 +1,14 @@
-export const jsonpRequest = (url, callback) => {
-    const callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
-    const fullUrl = `${url}&callback=${callbackName}`;
-    const script = document.createElement('script');
-    script.src = fullUrl;
+export const jsonpRequest = (url) => {
+    return new Promise((resolve) => {
+      const callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+      const fullUrl = `${url}&callback=${callbackName}`;
+      const script = document.createElement('script');
+      script.src = fullUrl;
   
-    window[callbackName] = (data) => {
-      callback(data);
-    };
+      window[callbackName] = (data) => {
+        resolve(data);
+      };
   
-    document.body.appendChild(script);
+      document.body.appendChild(script);
+    });
   };
